9bf8d28a39b37b488bfa99976fa2208cec2c164d
nara <dlskfko66@gmail.com>
2022-02-16 02:18:34 +0900

    file upload

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/movie">
+    <BrowserRouter basename={process.env.PUBLIC_UPL}>
       <App /> 
     </BrowserRouter>
   </React.StrictMode>,
